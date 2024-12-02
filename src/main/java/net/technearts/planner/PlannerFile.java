44222ea705e80b0009f99d17a095577a854cd223
Paulo Suderio <paulo.suderio@gmail.com>
2024-12-02 18:48:19 -0300

    PlannerFile gerando arquivos no formato correto.

--- src/main/java/net/technearts/planner/PlannerFile.java
+++ src/main/java/net/technearts/planner/PlannerFile.java
@@ -2,13 +2,18 @@ package net.technearts.planner;
 
 import io.quarkus.arc.log.LoggerName;
 import org.jboss.logging.Logger;
+import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.BaseConstructor;
 import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.representer.Representer;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -17,9 +22,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static java.nio.file.Files.newBufferedWriter;
-import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.Files.writeString;
+import static java.nio.file.StandardOpenOption.*;
+import static org.yaml.snakeyaml.DumperOptions.FlowStyle.FLOW;
 
+/**
+ * Manages all the reading and writing of files, be it yaml or text, but not application.yaml config file.
+ */
 public class PlannerFile {
     private final String directory;
     private final String prefix;
@@ -36,12 +45,23 @@ public class PlannerFile {
         this.suffix = suffix;
         this.month = month;
         this.year = year;
-        LoaderOptions options = new LoaderOptions();
-        this.yaml = new Yaml(new Constructor(Person.class, options));
+        LoaderOptions loaderOptions = new LoaderOptions();
+        DumperOptions dumperOptions = new DumperOptions();
+        //dumperOptions.setDefaultFlowStyle(FLOW);
+        //dumperOptions.setPrettyFlow(true);
+        //dumperOptions.setDereferenceAliases(true);
+        //dumperOptions.setAllowUnicode(true);
+        dumperOptions.setExplicitStart(true);
+        dumperOptions.setExplicitEnd(true);
+        Representer representer = new Representer(dumperOptions);
+        //representer.addClassTag(Person.class, Tag.MAP);
+        //representer.addClassTag(Float.class, Tag.FLOAT);
+        BaseConstructor constructor = new Constructor(Person.class, loaderOptions);
+        this.yaml = new Yaml(constructor, representer, dumperOptions, loaderOptions);
+
     }
 
     public String yamlFileName() {
-        //return prefix + String.format("%04d", year) + String.format("%02d", month) + ".yaml";
         return "%s%04d%02d.yaml".formatted(prefix, year, month);
     }
 
@@ -64,11 +84,13 @@ public class PlannerFile {
     public void dumpPeople(List<Timeslot> timeslots) {
         Path path = Path.of(directory, yamlFileName());
         timeslots.forEach(t -> t.getPerson().getScheduled().add(t.getMonthDay().getDayOfMonth()));
-        try (var writer = new PrintWriter(newBufferedWriter(path, StandardOpenOption.WRITE))) {
+        try(var writer = new StringWriter()) {
             yaml.dumpAll(timeslots.stream().map(Timeslot::getPerson).collect(Collectors.toSet()).iterator(), writer);
+            var result = writer.toString().replace("!!net.technearts.planner.Person", "");
+            result = result.replace("!!float ", "");
+            writeString(path, result, WRITE);
         } catch (IOException e) {
-            Log.error("Cannot find file %s".formatted(yamlFileName()));
-            System.exit(1);
+            throw new RuntimeException(e);
         }
     }
 
